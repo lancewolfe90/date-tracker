5000d34fe31c2cdf5232bf25e69fdad1bc5dfd09
Lance Wolfe <lwolfe@talentpath.com>
2022-03-23 15:58:58 -0400

    Expand birthday detail page

--- src/app/birthdays/birthday-detail/birthday-detail.component.ts
+++ src/app/birthdays/birthday-detail/birthday-detail.component.ts
@@ -1,5 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CalendarMathService } from 'src/app/calendar-math.service';
+import { GlobalStateService } from 'src/app/global-state.service';
+import { Birthday } from '../birthday.model';
 
 @Component({
   selector: 'app-birthday-detail',
@@ -8,10 +12,50 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BirthdayDetailComponent implements OnInit {
   currentId: number;
+  currentBirthday: Birthday;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private calendarMathService: CalendarMathService,
+    private globalStateService: GlobalStateService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.currentId = +this.route.snapshot.paramMap.get('id');
+    this.currentBirthday = this.globalStateService.lookupBirthday(
+      this.currentId
+    );
+  }
+
+  navigateBack(): void {
+    this.location.back();
+  }
+
+  calculateYearsPassed(date: string) {
+    return this.calendarMathService.getCurrentAge;
+  }
+
+  assembleMessageForNextOccurrence(date: string): string {
+    let assembledString = `Currently ${
+      this.calendarMathService.getCurrentAge(date).years
+    } years old.\nWill turn ${
+      this.calendarMathService.getCurrentAge(date).years + 1
+    } in`;
+    const nextOccurrence = this.calendarMathService.getNextOccurrence(date);
+    const timeUntilNextOccurrence = nextOccurrence
+      .diff(this.calendarMathService.rightNow, ['months', 'days'])
+      .toObject();
+    if (timeUntilNextOccurrence['months'] > 0) {
+      assembledString = assembledString.concat(
+        ' ' + Math.floor(timeUntilNextOccurrence['months']) + ' months'
+      );
+    }
+    if (timeUntilNextOccurrence['days'] > 0) {
+      assembledString = assembledString.concat(
+        ' ' + Math.floor(timeUntilNextOccurrence['days']) + ' days'
+      );
+    }
+    return assembledString;
   }
 }
