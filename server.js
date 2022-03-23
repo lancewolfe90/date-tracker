0fd065a2a7c27ba52fbe42eda52d5ca572ddce35
Lance Wolfe <lwolfe@talentpath.com>
2022-03-23 15:18:08 -0400

    Add server.js and update node start script

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/capstone-birthdays"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/capstone-birthdays/index.html"));
+});
+
+app.listen(port);
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
+// TODO Add splash screen
+// TODO Make it offline-FIRST, not offline-ONLY
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
-
 }
